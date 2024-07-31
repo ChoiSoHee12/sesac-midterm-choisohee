fe65cadf1697215c991567d4a65d413a4555ff69
ChoiSoHee12 <chlthgml0102@gmail.com>
2024-07-31 18:40:50 +0900

    456

--- backend/index.sql
+++ backend/index.sql
@@ -3,7 +3,11 @@ create table todo (
 id int auto_increment primary key not null,
 title  varchar(255) not null,
 done not null 
+createdAt datetime not null default CURRENT_TIMESTAMP,
+updatedAt datetime not null default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 
 )
 
+
 desc todo;
+    
